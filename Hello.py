206744cb11249f2e1718a1b3361375ebefdad78f
manjula <manjulasingh46@gmail.com>
2021-12-24 17:11:50 +0530

    Pipeline test for python

--- Hello.py
+++ Hello.py
@@ -1,3 +1,10 @@
 # This program prints Hello, world!
 
 print("Hello, world!")
+
+# Initialize the list
+weekdays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"]
+print("Seven Weekdays are:\n")
+# Iterate the list using for loop
+for day in range(len(weekdays)):
+    print(weekdays[day])
